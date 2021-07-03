fca0a8f958e098f2acb0462ee4d759f14034bddd
Akash Kumar <akaysharma894@gmail.com>
2021-07-04 00:45:39 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,49 +2,24 @@ import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const getWords = () =>
-  `Last month a grand exhibition was held in our city. My friends and I went to see it in evening.
-
-  Our first impression on entering the grounds was that whole thing looked like a fairyland. The vast space was decorated in magnificent, bright and purple colour and lit up with countless lights. Men, women and children were moving from corner to corner, admiring the beauty of all kinds of stalls set up. These stalls were like small shops.
-  
-  While the stalls made a very interesting sight, what attracted us most was the Children's Corner in the exhibition. The Children's Corner was crowded with boys and girls. All types of amusements could be seen here. Children and some grown-ups were enjoying the giant wheel, wooden hoses, dodge-cars, railway train and other things. I too had my share of fun with my friends and returned home after enjoying a most delightful evening. 
+  `Last month a grand exhibit interesting sight, what attracted us most was the Children's Corner in the exhibition. The Children's Corner was crowded with boys and girls. All types of amusements could be seen here. Children and some grown-ups were enjoying the giant wheel, wooden hoses, dodge-cars, railway train and other things. I too had my share of fun with my friends and returned home after enjoying a most delightful evening. 
    `.split(" ");
 
 function WordData(props) {
   const { text, active, correct } = props;
 
-  const rerender = useRef(0);
-  useEffect(() => {
-    rerender.current += 1;
-  });
-
   if (correct === true) {
-    return (
-      <span className="correct">
-        {text} ({rerender.current}){" "}
-      </span>
-    );
+    return <span className="correct">{text} </span>;
   }
   if (correct === false) {
-    return (
-      <span className="incorrect">
-        {text} ({rerender.current}){" "}
-      </span>
-    );
+    return <span className="incorrect">{text} </span>;
   }
 
   if (active) {
-    return (
-      <span className="active">
-        {text} ({rerender.current}){" "}
-      </span>
-    );
+    return <span className="active">{text} </span>;
   }
 
-  return (
-    <span>
-      {text} ({rerender.current}){" "}
-    </span>
-  );
+  return <span>{text} </span>;
 }
 
 WordData = React.memo(WordData);
@@ -56,19 +31,30 @@ function Timer(props) {
   const { correctWords, startCounting } = props;
 
   useEffect(() => {
+    let id;
     if (startCounting) {
-      setInterval(() => {
+      id = setInterval(() => {
         setTimeElapsed((oldTime) => oldTime + 1);
       }, 1000);
     }
+    return () => {
+      clearInterval(id);
+    };
   }, [startCounting]);
 
   const minutes = timeElapsed / 60;
 
   return (
-    <div>
-      <span>Time : {timeElapsed}</span>
-      <p>W.P.M : {(correctWords / minutes || 0).toFixed(2)}</p>
+    <div className="timer">
+      <span>
+        <b>Time :</b> {timeElapsed}
+      </span>
+      <span>
+        <b>WPM :</b> {(correctWords / minutes || 0).toFixed(2)}
+      </span>
+      <span>
+        <b>Incorrect words :</b> {!correctWords}
+      </span>
     </div>
   );
 }
@@ -81,10 +67,18 @@ function App() {
   const [correctWordArray, setCorrectWordArray] = useState([]);
 
   const processInput = (value) => {
+    if (activeWordIndex === words.current.length - 1) {
+      return;
+    }
     if (!startCounting) {
       setStartCounting(true);
     }
     if (value.endsWith(" ")) {
+      if (activeWordIndex === words.current.length - 1) {
+        setStartCounting(false);
+      } else {
+        setUserInput("completed");
+      }
       setActiveWordIndex((index) => index + 1);
       setUserInput(" ");
       setStartCounting(true);
@@ -103,23 +97,26 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Typing test </h1>
+      <h1 className="title">Typing test </h1>
       <Timer
         startCounting={startCounting}
         correctWords={correctWordArray.filter(Boolean).length}
       />
-      <p>
-        {words.current.map((word, index) => {
-          return (
-            <WordData
-              text={word}
-              active={index === activeWordIndex}
-              correct={correctWordArray[index]}
-            />
-          );
-        })}
-      </p>
+      <div className="paragrafh-wrapper">
+        <p>
+          {words.current.map((word, index) => {
+            return (
+              <WordData
+                text={word}
+                active={index === activeWordIndex}
+                correct={correctWordArray[index]}
+              />
+            );
+          })}
+        </p>
+      </div>
       <input
+        className="input-area"
         type="text"
         value={userInput}
         onChange={(e) => processInput(e.target.value)}
